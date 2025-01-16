3418c722dceec5a07475b6257f1a770c6521f7da
yk-lab <yk-lab@users.noreply.github.com>
2025-01-16 09:26:57 +0900

    fix: add error handling for API fetch in item and screen save image handlers

--- server/api/screen-save-images.ts
+++ server/api/screen-save-images.ts
@@ -9,5 +9,7 @@ export default defineEventHandler(async () => {
 
   return await $fetch<ScreenSaveImage[]>(runtimeConfig.screenSaveImageApiUrl, {
     responseType: "json",
+  }).catch((e) => {
+    throw e;
   });
 })
