3ff1624d5ff181e1f9fa3e892216557f720842ff
yk / k.yamada <yk-lab@users.noreply.github.com>
2025-01-18 00:09:35 +0900

    fix: handle missing runtime configuration for item and screen save image APIs (#47)

--- server/api/items/[janCode].ts
+++ server/api/items/[janCode].ts
@@ -2,11 +2,18 @@ import type { Item } from '../../../schemas/item';
 
 export default defineEventHandler(async (event) => {
     const runtimeConfig = useRuntimeConfig();
+    if (!runtimeConfig.itemApiUrl) {
+        console.error("itemApiUrl is not defined in runtime config");
+        throw createError({
+            statusCode: 500,
+            statusMessage: 'Internal Server Error',
+        });
+    }
 
     const janCode = getRouterParam(event, 'janCode');
-    const data = runtimeConfig.itemApiUrl ? await $fetch<Item[]>(runtimeConfig.itemApiUrl, {
+    const data = await $fetch<Item[]>(runtimeConfig.itemApiUrl, {
         responseType: "json",
-    }) : [];
+    });
 
     const item = data.find((item) => item.jan_code === janCode);
     if (!item) {
